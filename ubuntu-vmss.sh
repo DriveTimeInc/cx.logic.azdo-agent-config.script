ca91d21d9e6e2ff40707fb19362786a1bcf205ce
Michael Foster <32286306+MBFrosty@users.noreply.github.com>
2022-09-20 11:28:51 -0700

    Cleaned it up

--- ubuntu-vmss.sh
+++ ubuntu-vmss.sh
@@ -1,34 +1,42 @@
 sudo apt update
-# Update the list of packages
-sudo apt-get update
+sudo apt upgrade
+
 # Install pre-requisite packages.
-sudo apt-get install \
+sudo apt install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release \
     apt-transport-https \
     software-properties-common \
+
 # Download the Microsoft repository GPG keys
 wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+
 # Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
+
 # Update the list of packages after we added packages.microsoft.com
-sudo apt-get update
-# Install PowerShell
-sudo apt-get install -y powershell
-
-
-
-sudo apt-get remove docker docker-engine docker.io containerd runc
-
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` stable"
-
 sudo apt update
 
-sudo apt install moby-engine moby-cli containerd.io
+# Install PowerShell
+sudo apt install -y powershell
 
-sudo service docker start
+
+# Add the GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add the repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Update repository
+sudo apt update
+
+# Set to docker repo
+apt-cache policy docker-ce
+
+# Install docker-ce
+sudo apt install docker-ce
+
+# Check docker status
+sudo systemctl status docker
