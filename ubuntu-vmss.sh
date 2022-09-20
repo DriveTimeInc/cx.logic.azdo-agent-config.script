d190c1ad3436281b2730d6bf967beecad9e3c825
Michael Foster <32286306+MBFrosty@users.noreply.github.com>
2022-09-19 17:31:01 -0700

    trying this

--- ubuntu-vmss.sh
+++ ubuntu-vmss.sh
@@ -21,16 +21,14 @@ sudo apt-get install \
 
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
-sudo mkdir -p /etc/apt/keyrings
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-echo \
-"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` stable"
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io
 
 sudo service docker start
