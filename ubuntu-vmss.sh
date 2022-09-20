cb2c1ca0c01d4fcd898245eb9fe7b64b4488543e
Michael Foster <32286306+MBFrosty@users.noreply.github.com>
2022-09-19 17:08:58 -0700

    Update ubuntu-vmss.sh

--- ubuntu-vmss.sh
+++ ubuntu-vmss.sh
@@ -28,3 +28,5 @@ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo service docker start
+
+systemctl start docker
