422da81ec2de312ce4d6220e2a648bdf453b653b
Dan Anderson <106992569+dandrivetime@users.noreply.github.com>
2023-12-21 14:26:55 -0500

    Add argo rollouts install

--- ubuntu-vmss.sh
+++ ubuntu-vmss.sh
@@ -42,6 +42,10 @@ apt-cache policy docker-ce
 # Install docker-ce
 sudo apt-get install -y docker-ce
 
-
 # Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Install argo rollouts
+curl -LO "https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-linux-amd64"
+chmod +x ./kubectl-argo-rollouts-linux-amd64
+mv ./kubectl-argo-rollouts-linux-amd64 /usr/local/bin/kubectl-argo-rollouts
