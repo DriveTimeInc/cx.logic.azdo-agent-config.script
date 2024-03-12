6e8f0586f1dddcc8541cb2a8e602a2c3f8341487
Kenneth Abrams <Kenneth.Abrams@drivetime.com>
2024-03-12 10:31:38 -0700

    Removing var

--- ubuntu-vmss.sh
+++ ubuntu-vmss.sh
@@ -49,5 +49,3 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 curl -LO "https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-linux-amd64"
 chmod +x ./kubectl-argo-rollouts-linux-amd64
 mv ./kubectl-argo-rollouts-linux-amd64 /usr/local/bin/kubectl-argo-rollouts
-
-export VSTS_AGENT_INPUT_POOL="On-Prem Ubuntu"
