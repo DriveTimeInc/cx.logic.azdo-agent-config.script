a97d1b846e0083f8e90a5defff4900e4585ab248
Anthony Botts <anthony.botts@drivetime.com>
2026-05-06 10:10:42 -0700

    Update Ubuntu sources to use HTTPS

--- ubuntu-vmss.sh
+++ ubuntu-vmss.sh
@@ -1,3 +1,5 @@
+echo "Starting Installation 5/6/26"
+
 sudo sed -i 's|http://azure.archive.ubuntu.com|https://azure.archive.ubuntu.com|g' /etc/apt/sources.list
 sudo sed -i 's|http://security.ubuntu.com|https://security.ubuntu.com|g' /etc/apt/sources.list
 
