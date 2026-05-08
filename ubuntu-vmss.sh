becace754c543213777071642ee8b77dc7260489
Van Patterson <44014448+groodymoloko@users.noreply.github.com>
2026-05-08 08:49:07 -0700

    changing back to us archive

--- ubuntu-vmss.sh
+++ ubuntu-vmss.sh
@@ -1,6 +1,6 @@
 echo "Starting Installation 5/6/26"
 
-#sudo sed -i 's|http://azure.archive.ubuntu.com|http://us.archive.ubuntu.com|g' /etc/apt/sources.list
+sudo sed -i 's|http://azure.archive.ubuntu.com|http://us.archive.ubuntu.com|g' /etc/apt/sources.list
 #sudo sed -i 's|http://security.ubuntu.com|http://security.ubuntu.com|g' /etc/apt/sources.list
 
 sudo apt-get update
